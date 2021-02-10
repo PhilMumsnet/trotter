78f9c627a6e29abedcedd13e05f9a7cb88886567
Phil Vigus <phil.vigus@mumsnet.com>
2021-02-10 09:10:01 +0000

    Add a simple header

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,9 @@
 <x-layouts.master :title="$title ?? null" :description="$description ?? null">
-    <div>
-        {{ $slot }}
-    </div>
+        <header class="bg-gray-800 text-white p-8 flex items-center">
+            <img class="h-24 mr-10" src="/images/trotter-logo.svg"/>
+            <h1 class="text-8xl">Trotter</h1>
+        </header>
+        <main class="bg-green-200">
+            {{ $slot }}
+        </main>
 </x-layouts.master>
