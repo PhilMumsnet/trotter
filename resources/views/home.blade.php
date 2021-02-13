cb183fb7ada8ce37377d8486e2fd2289607886c9
Phil Vigus <phil.vigus@mumsnet.com>
2021-02-13 14:16:08 +0000

    Add trott time to display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.three-columns :title="$title ?? null" :description="$description ?? null">
     <div>
         @foreach ($trotts as $trott)
-            <p>{{ $trott->user->name}} - {{ $trott->body }}</p>
+            <p>{{ $trott->user->name}} - {{ $trott->body }} - {{ $trott->updated_at->diffForHumans() }}</p>
         @endforeach
     </div>
 </x-layouts.three-columns>
