869feef2b669befe950decc9bf7606321e2ca3c0
Phil Vigus <phil.vigus@mumsnet.com>
2021-02-15 08:15:21 +0000

    Create (very) basic new trott form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.three-columns :title="$title ?? null" :description="$description ?? null">
     <div>
+        <livewire:new-trott-form />
+        
         @foreach ($trotts as $trott)
             <x-trott-summary :trott="$trott" />
         @endforeach
---
+++ resources/views/livewire/new-trott-form.blade.php
@@ -0,0 +1,5 @@
+<div class="h-60 p-6 rounded-3xl border-black border-2 bg-white flex flex-col">
+    <textarea class="w-full resize-none h-full">
+    </textarea>
+    <button class="bg-red-300">Trotts away!!</button>
+</div>
