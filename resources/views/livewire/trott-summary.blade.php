ddf80989be496d905d4f80f2471383ca54749fc6
Phil Vigus <phil.vigus@mumsnet.com>
2021-03-01 15:15:38 +0000

    Fix formatting

--- resources/views/livewire/trott-summary.blade.php
+++ resources/views/livewire/trott-summary.blade.php
@@ -1,6 +1,6 @@
 <div class="border-black-400 border-b-2 p-2">
     <h2>
-        <span class="font-bold">{{ $trott->user->name}}</span>
+        <span class="font-bold">{{ $trott->user->name }}</span>
         <span class="text-gray-400"> - {{ $trott->updated_at->longAbsoluteDiffForHumans() }}</span>
     </h2>
     <p class="prose">
