412d9672016770f3ecda0b8da85312fbfab2138d
Phil Vigus <phil.vigus@mumsnet.com>
2021-03-01 16:44:08 +0000

    Add logout button

--- resources/views/components/partials/_sidebar.blade.php
+++ resources/views/components/partials/_sidebar.blade.php
@@ -5,5 +5,17 @@
         <li>
             <a class="font-bold text-5xl mt-4 block" href="{{ route('home') }}">Home</a>
         </li>
+
+        <li>
+            <a class="font-bold text-5xl mt-4 block" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+        </li>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
     </ul>
 </nav>
