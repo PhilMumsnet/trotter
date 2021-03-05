33e74f1b40b9c4657bdfec63061c15d272a3fe2b
Phil Vigus <phil.vigus@mumsnet.com>
2021-03-05 08:41:51 +0000

    Style login and registration pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,12 @@
 <x-layouts.whole-page :title="$title ?? null" :description="$description ?? null">
     <div class="flex h-screen justify-center items-center">
-        <div class="border-2 border-gray w-1/2">
-            <div class="p-2 bg-gray-200">Login</div>
+        <div class="border-2 border-gray w-2/5">
+            <div class="p-2 bg-gray-200 flex items-center">
+                <img class="h-16 m-2" src="/images/trotter-logo.svg"/>
+                <h1 class="text-5xl font-bold inline ml-2">Trotter Login</h1>
+            </div>
 
-            <div class="p-2">
+            <div class="p-4">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
@@ -11,7 +14,7 @@
                         <label for="email">E-Mail</label>
 
                         <div>
-                            <input id="email" type="email" class="w-full border-2 border-gray mt-1 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="w-full p-2 border-2 border-gray mt-1 text-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -25,7 +28,7 @@
                         <label for="password">Password</label>
 
                         <div>
-                            <input id="password" type="password" class="w-full border-2 border-gray mt-1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <input id="password" type="password" class="w-full p-2 border-2 border-gray mt-1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -44,7 +47,7 @@
                     </div>
 
                     <div class="mt-1">
-                        <button class="w-full bg-red-300" type="submit">
+                        <button class="w-full bg-red-300 p-2" type="submit">
                             Submit
                         </button>
 
