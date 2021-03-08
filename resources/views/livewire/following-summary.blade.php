3caa4cc241a38a91f667e749ff21755606dbc615
Phil Vigus <phil.vigus@mumsnet.com>
2021-03-08 11:53:41 +0000

    Add user links to following lists

--- resources/views/livewire/following-summary.blade.php
+++ resources/views/livewire/following-summary.blade.php
@@ -1,11 +1,13 @@
 <div class="flex items-center mt-2">
-    <img class="rounded-full mr-2" src="{{ Gravatar::src($user->email, 50) }}">
-    <div  class="flex-1">
-        <div class="font-bold">
-            {{ $user->name }}
+    <a class="flex-1 flex" href="{{ route('user', $user->username) }}">
+        <img class="rounded-full mr-2" src="{{ Gravatar::src($user->email, 50) }}">
+        <div  class="flex-1">
+            <div class="font-bold">
+                {{ $user->name }}
+            </div>
+            <div class="text-gray-500">
+                &#64;{{ $user->username }}
+            </div>
         </div>
-        <div class="text-gray-500">
-            &#64;{{ $user->username }}
-        </div>
-    </div>
+    </a>
 </div>
